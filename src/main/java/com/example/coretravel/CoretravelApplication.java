9b48c16a5aeea605e4b3afb235ddaaa48436626f
henryjaehun <noframed@gmail.com>
2024-11-22 20:41:40 +0900

    First commit

---
+++ src/main/java/com/example/coretravel/CoretravelApplication.java
@@ -0,0 +1,13 @@
+package com.example.coretravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoretravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoretravelApplication.class, args);
+    }
+
+}
