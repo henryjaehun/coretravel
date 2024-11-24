f44f4ed75d1510be81b732ac1786e887e58583c1
henryjaehun <noframed@gmail.com>
2024-11-24 23:08:17 +0900

    add msg format

--- src/main/java/com/example/coretravel/service/MsgGenerateService.java
+++ src/main/java/com/example/coretravel/service/MsgGenerateService.java
@@ -30,13 +30,12 @@ public class MsgGenerateService {
 
 
     // 2. 가이드를 이름으로 조회하여 데이터베이스에서 가져오는 메소드
-    public String getGuideInfoByName(String guideName) {
+    public Guide getGuideInfoByName(String guideName) {
         // 레포지토리에서 이름으로정보조회함
 
         // 없을 경우 예외처리
         return guideRepository.findByName(guideName)
-                .orElseThrow(() -> new RuntimeException("Guide not found for name: " + guideName))
-                .toString();
+                .orElseThrow(() -> new RuntimeException("Guide not found for name: " + guideName));
 
     }
 
@@ -44,7 +43,39 @@ public class MsgGenerateService {
     private String createMessage(String clientName, String tourDate,
                                String pickupLocation, String pickupTime, Guide guide){
         // 내용 생성 필요
-        return null;
+        return String.format("""
+                Dear. %s
+                Warm greetings from Core Travel!
+                We are happy to have you for the Day tour! Below is the information.
+                
+                Tour: UNESCO Day Tour West Course
+                Date: %s
+                Pick-up Time: %s (Please be ready 5 minutes earlier)
+                ※ The bus/van won’t wait for your tardiness of arrival.
+                Pick-up Place: %s
+                (Please wait outside the hotel's main entrance)
+                Guide Name: %s
+                Guide Contact: %s
+                Vehicle: %s
+                
+                ※ You can contact our guide directly on the tour day should you have any inquiries via WhatsApp link below:
+                https://api.whatsapp.com/send?phone=%s&text=
+                ※ Please make sure to have appropriate footwear for the hike.
+                ※ Please reply to us once you check this for confirmation.
+                ※ Tour course is subject to change according to local circumstances such as seasonal events/weather/road conditions/etc.
+                
+                Best regards,
+                %s
+                """,
+                clientName,
+                tourDate,
+                pickupTime,
+                pickupLocation,
+                guide.getGuidename(),
+                guide.getPhone(),
+                guide.getVehicle(),
+                guide.getPhone(),
+                guide.getGuidename());
     }
 
 
